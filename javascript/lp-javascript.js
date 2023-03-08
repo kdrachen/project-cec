09ae73919c1ec2d90adecead311be5a05944b647
xiaohuolong07 <natan_will88@hotmail.com>
2023-03-08 17:17:09 -0300

    Contador inserido 08/03/2023

--- javascript/lp-javascript.js
+++ javascript/lp-javascript.js
@@ -5,3 +5,32 @@ faqs.forEach((faq) => {
         faq.classList.toggle('active');
     });
 });
+
+var minutos = 7;
+var segundos = 59;
+
+function atualizaContador() {
+  var minutosElemento = document.getElementById("minutos");
+  var segundosElemento = document.getElementById("segundos");
+
+  // Subtrai um segundo dos minutos e segundos restantes
+  if (segundos > 0) {
+    segundos--;
+  } else if (minutos > 0) {
+    minutos--;
+    segundos = 59;
+  } else {
+    // Acabou o tempo, para a contagem
+    clearInterval(intervalId);
+    alert("Tempo acabou!");
+    return;
+  }
+
+  // Atualiza os elementos HTML com o novo valor
+  var segundosTexto = segundos < 10 ? "0" + segundos : segundos;
+  minutosElemento.innerHTML = minutos;
+  segundosElemento.innerHTML = segundosTexto;
+}
+
+// Inicia a contagem
+var intervalId = setInterval(atualizaContador, 1000);
